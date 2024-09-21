b07d074c55bfa40eb0fe485339e3f679bc0c7356
nicolasgghg <nicolasgghg12@gmail.com>
2024-09-21 18:09:38 -0300

    Feat: Initial Settings

---
+++ src/app/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-blue-400 text-5xl">Hello World!</p>
+    </div>
+  );
+
+}
